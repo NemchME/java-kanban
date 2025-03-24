86f95d73d283b917322a94ac8904f2ce508eec67
NemchME <mnemch@yandex.ru>
2025-03-24 13:50:12 +0300

    Creating an epic

---
+++ src/Epic.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class Epic extends Task {
+    private final ArrayList<Integer> subtaskIds = new ArrayList<>();
+
+    public Epic(String name, String description) {
+        super(name, description, Status.NEW);
+    }
+
+    public ArrayList<Integer> getSubtaskIds() {
+        return subtaskIds;
+    }
+
+    public void addSubtaskId(int subtaskId) {
+        subtaskIds.add(subtaskId);
+    }
+
+    public void removeSubtaskId(int subtaskId) {
+        subtaskIds.remove((Integer) subtaskId);
+    }
+
+    @Override
+    public String toString() {
+        return "Epic{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", id=" + id +
+                ", status=" + status +
+                ", subtaskIds=" + subtaskIds +
+                '}';
+    }
+}
