57377f3159e0168a891c4ec1897676b5f6c6160f
NemchME <mnemch@yandex.ru>
2025-03-24 13:53:45 +0300

    Creating a subtask

--- src/Subtask.java
+++ src/Subtask.java
@@ -1,2 +1,27 @@
-public class Subtask {
+public class Subtask extends Task{
+    private int epicId;
+
+    public Subtask(String name, String description, Status status, int epicId) {
+        super(name, description, status);
+        this.epicId = epicId;
+    }
+
+    public int getEpicId() {
+        return epicId;
+    }
+
+    public void setEpicId(int epicId) {
+        this.epicId = epicId;
+    }
+
+    @Override
+    public String toString() {
+        return "Subtask{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", id=" + id +
+                ", status=" + status +
+                ", epicId=" + epicId +
+                '}';
+    }
 }
