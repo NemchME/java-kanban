d6daae7977c5259c16eaf7bc7df913237d1f6c4d
NemchME <mnemch@yandex.ru>
2025-03-24 13:38:28 +0300

    Creating a Task

---
+++ src/Task.java
@@ -0,0 +1,12 @@
+public class Task {
+    protected String name;
+    protected String description;
+    protected int id;
+    protected Status status;
+
+    public Task(String name, String description, Status status) {
+        this.name = name;
+        this.description = description;
+        this.status = status;
+    }
+}
